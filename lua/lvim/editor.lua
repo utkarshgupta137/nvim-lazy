aeae2c2e9c3b3cc8c0238ceb1a031d6687231623
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-07-17 16:30:20 +0100

    editor: disable flash.nvim

--- lua/lvim/editor.lua
+++ lua/lvim/editor.lua
@@ -182,8 +182,7 @@ return {
     },
   },
 
-  { "ggandor/flit.nvim", enabled = false },
-  { "ggandor/leap.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 
   {
     "folke/which-key.nvim",
