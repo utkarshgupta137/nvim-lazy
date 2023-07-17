9858fc14d88499d6b79b22fd643b2cbe3cd0ba10
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-07-17 16:35:20 +0100

    plugins/frecency: update config

--- lua/plugins/telescope-frecency.lua
+++ lua/plugins/telescope-frecency.lua
@@ -1,17 +1,9 @@
 return {
   "nvim-telescope/telescope-frecency.nvim",
   dependencies = { "kkharji/sqlite.lua" },
-  init = function() require("telescope").load_extension("frecency") end,
   keys = {
     { "<leader>fr", "<cmd>Telescope frecency<cr>", desc = "frecency (root dir)" },
     { "<leader>fR", "<cmd>Telescope frecency workspace=CWD<cr>", desc = "frecency (cwd)" },
   },
-  opts = {
-    workspaces = {
-      ["nvim"] = "~/.config/nvim",
-      ["tfx"] = "~/tensorfox",
-      ["nix"] = "~/utkarsh/nix",
-    },
-    db_safe_mode = true,
-  },
+  config = function() require("telescope").load_extension("frecency") end,
 }
