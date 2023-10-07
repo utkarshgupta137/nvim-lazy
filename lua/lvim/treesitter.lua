e780c399fcb135a0129b13c57c2285b644cdd0de
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-10-07 14:12:06 +0100

    treesitter: add `comment` to ensure_installed

--- lua/lvim/treesitter.lua
+++ lua/lvim/treesitter.lua
@@ -3,6 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     ---@type TSConfig
     opts = {
+      ensure_installed = { "comment" },
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
