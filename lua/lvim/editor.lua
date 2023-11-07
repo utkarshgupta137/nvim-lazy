744c3dac112ad57c66efdf9816d55ebbf659884f
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-11-07 06:24:52 +0000

    editor/neo-tree: update copy_selector

--- lua/lvim/editor.lua
+++ lua/lvim/editor.lua
@@ -115,33 +115,32 @@ return {
           local filename = node.name
           local modify = vim.fn.fnamemodify
 
-          local results = {
-            { key = "a", val = filepath, msg = "Absolute path" },
-            { key = "h", val = modify(filepath, ":~"), msg = "Path relative to Home" },
-            { key = "r", val = modify(filepath, ":."), msg = "Path relative to CWD" },
-            { key = "f", val = filename, msg = "Filename" },
-            { key = "e", val = modify(filename, ":r"), msg = "Filename w/o extension" },
+          local vals = {
+            ["BASENAME"] = modify(filename, ":r"),
+            ["EXTENSION"] = modify(filename, ":e"),
+            ["FILENAME"] = filename,
+            ["PATH (CWD)"] = modify(filepath, ":."),
+            ["PATH (HOME)"] = modify(filepath, ":~"),
+            ["PATH"] = filepath,
+            ["URI"] = vim.uri_from_fname(filepath),
           }
 
-          local messages = {
-            { "\nChoose to copy to clipboard:\n", "Normal" },
-          }
-          for _, result in ipairs(results) do
-            if result.val and result.val ~= "" then
-              vim.list_extend(messages, {
-                { ("%s."):format(result.key), "Identifier" },
-                { (" %s: "):format(result.msg) },
-                { result.val, "String" },
-                { "\n" },
-              })
+          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+          if vim.tbl_isempty(options) then
+            vim.notify("No values to copy", vim.log.levels.WARN)
+            return
+          end
+          table.sort(options)
+          vim.ui.select(options, {
+            prompt = "Choose to copy to clipboard:",
+            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+          }, function(choice)
+            local result = vals[choice]
+            if result then
+              vim.notify(("Copied: `%s`"):format(result))
+              vim.fn.setreg("+", result)
             end
-          end
-          vim.api.nvim_echo(messages, false, {})
-          local result = results[vim.fn.getcharstr()]
-          if result and result.val and result.val ~= "" then
-            vim.notify("Copied: " .. result.val)
-            vim.fn.setreg("+", result.val)
-          end
+          end)
         end,
       },
     },
