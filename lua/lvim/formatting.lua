d1bd727705cc98463d689cfd5b43c54a742732b6
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-11-08 03:06:47 +0000

    formatting: use 2 spaces for shfmt

--- lua/lvim/formatting.lua
+++ lua/lvim/formatting.lua
@@ -11,5 +11,13 @@ return {
       -- have other formatters configured.
       ["_"] = { "dprint" },
     },
+    -- Custom formatters and changes to built-in formatters
+    formatters = {
+      shfmt = {
+        -- The base args are { "-filename", "$FILENAME" } so the final args will be
+        -- { "-i", "2", "-filename", "$FILENAME" }
+        prepend_args = { "-i", "2" },
+      },
+    },
   },
 }
