69fd40ac84cfdb3b71f9982811249f19b0711534
Utkarsh Gupta <utkarshgupta137@gmail.com>
2024-05-20 14:44:49 +0100

    keymaps: update command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,16 +4,7 @@
 
 local Util = require("lazyvim.util")
 
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
+local map = vim.keymap.set
 
 -- motions
 map({ "n", "v" }, "0", "_")
