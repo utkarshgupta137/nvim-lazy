45117141f7ba2574b7a4b46f20e42cc5b291c5ac
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-01-16 17:26:44 +0000

    keymaps: add overrides

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map({ "n", "v" }, "gg", "gg_")
+map({ "n", "v" }, "n", "nzz")
+map({ "n", "v" }, "N", "Nzz")
+map({ "n", "v" }, "<C-u>", "zz<C-u>")
+map({ "n", "v" }, "<C-d>", "zz<C-d>")
