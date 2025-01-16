6f68759189ee8f98a870274bf4d689d50027f777
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-01-16 17:26:52 +0000

    options: add overrides

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.snacks_animate = false
+
+local opt = vim.opt
+
+opt.colorcolumn = "101"
+opt.sessionoptions = "buffers,curdir,folds,globals,tabpages,winpos,winsize"
+opt.signcolumn = "yes:2"
