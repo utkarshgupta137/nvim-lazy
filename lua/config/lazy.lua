bf5d95f2649d4cc753ed7938d48eb5d983b47f8c
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-07-27 18:26:36 +0100

    lazy: add typescript lang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,6 +38,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
