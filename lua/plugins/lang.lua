b93e665ec97bc3660ebfc25f09018efcbce72f25
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-01-15 17:42:42 +0000

    lang/rust-analyzer: update settings

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -5,17 +5,39 @@ return {
       server = {
         default_settings = {
           ["rust-analyzer"] = {
+            assist = {
+              preferSelf = true,
+            },
+            hover = {
+              memoryLayout = {
+                alignment = "both",
+                offset = "both",
+                padding = "both",
+                size = "both",
+              },
+              show = {
+                enumVariants = 8,
+                fields = 8,
+                traitAssocItems = 3,
+              },
+            },
             imports = {
               granularity = {
                 enforce = true,
                 group = "module",
               },
               preferPredule = true,
-              prefix = "crate",
             },
             references = {
               excludeImports = true,
               excludeTests = true,
+            },
+            workspace = {
+              symbol = {
+                search = {
+                  excludeImports = true,
+                },
+              },
             },
           },
         },
