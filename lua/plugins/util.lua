dd85d21f54d4b2d04ba7f2a0e885e3c4fdacaba8
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-01-15 17:40:24 +0000

    util/persisted: update keys

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -8,10 +8,10 @@ return {
     "olimorris/persisted.nvim",
     lazy = false,
     keys = {
-      { "<leader>qs", "<cmd>SessionLoad<cr>", desc = "Restore Session" },
-      { "<leader>qS", "<cmd>SessionSelect<cr>", desc = "Select Session" },
-      { "<leader>ql", "<cmd>SessionLoadLast<cr>", desc = "Restore Last Session" },
-      { "<leader>qd", "<cmd>SessionStop<cr>", desc = "Don't Save Current Session" },
+      { "<leader>qs", "<cmd>Persisted load<cr>", desc = "Restore Session" },
+      { "<leader>qS", "<cmd>Persisted select<cr>", desc = "Select Session" },
+      { "<leader>ql", "<cmd>Persisted load_last<cr>", desc = "Restore Last Session" },
+      { "<leader>qd", "<cmd>Persisted stop<cr>", desc = "Don't Save Current Session" },
     },
     opts = {
       save_dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"),
